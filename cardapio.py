cae1df11a8995ce7b7b5df6bd9bc825be71a1604
pietradassalin <pietradassalin@gmail.com>
2025-11-18 23:35:35 +0000

    enviando arquivos

---
+++ cardapio.py
@@ -0,0 +1,11 @@
+def buscar_cardapio():
+    itens = [
+        {
+        "id": 1,
+        "nome": "Chocomoça",
+        "descrição": "Bolo de chocolate com ninho",
+        "preco": 49.99,
+        "foto": ""
+        }
+    ]
+    return itens
